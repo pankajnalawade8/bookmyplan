a7fab5093f2694d61f5794d00fc209030522cd26
pankajnalawade8 <pankajnalawasw8@gmail.com>
2026-01-28 10:39:50 +0530

    Confirmed working Railways endpoint

---
+++ src/main/java/com/example/bookmypan/Railways.java
@@ -0,0 +1,12 @@
+package com.example.bookmypan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class BookmypanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmypanApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/bookmypan/BookmypanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookmypan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmypanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
