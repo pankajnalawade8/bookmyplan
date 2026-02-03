4f3e8b860ae1316e1dcd788c0246f4eed2928cc9
pankajnalawade8 <pankajnalawasw8@gmail.com>
2026-02-03 15:21:57 +0530

    new class

--- src/main/java/com/example/bookmypan/bus.java
+++ src/main/java/com/example/bookmypan/bus.java
@@ -1,4 +1,13 @@
 package com.example.bookmypan;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class bus {
+
+    @GetMapping("/bus")
+    public String getData() {
+        return "Hello Welcome to BookMyPlan, Please book bus tickets at 10% discount";
+    }
 }
